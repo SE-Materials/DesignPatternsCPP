8163a21f204d9ec73a4f5fd96216d0251940e23c
Aniket Bhushan <aniket2701bhushan@gmail.com>
2019-10-14 23:13:30 +0530

    Initial Commit

---
+++ DesignPatterns/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  DesignPatterns
+//
+//  Created by Aniket Bhushan on 14/10/19.
+//  Copyright © 2019 Aniket Bhushan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
